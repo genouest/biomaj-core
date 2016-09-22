dcde005077434f92068ff1e17daa795349b65629
Olivier Sallou <olivier.sallou@irisa.fr>
2016-09-22 12:21:02 +0200

    add missing future dep

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ config = {
     'url': 'http://biomaj.genouest.org',
     'download_url': 'http://biomaj.genouest.org',
     'author_email': 'olivier.sallou@irisa.fr',
-    'version': '3.0.1',
+    'version': '3.0.2',
      'classifiers': [
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -45,6 +45,7 @@ config = {
     ],
     'install_requires': ['nose',
                          'mock',
+                         'future'
                          ],
     'packages': find_packages(),
     'include_package_data': True,
