133443f242d9c93536cc7ebb1e8c32e347fd761c
Olivier Sallou <olivier.sallou@irisa.fr>
2020-01-28 18:36:46 +0100

    Remove py 3.4 Closes #8

--- setup.py
+++ setup.py
@@ -37,8 +37,7 @@ config = {
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3'
     ],
     'install_requires': [
                          'future',
