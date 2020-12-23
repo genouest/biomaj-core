258eec99ecc51a6b90513ba39adc0dfda8b46822
Olivier Sallou <olivier.sallou@irisa.fr>
2020-12-23 06:55:55 +0100

    prepare future release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ config = {
     'url': 'http://biomaj.genouest.org',
     'download_url': 'http://biomaj.genouest.org',
     'author_email': 'olivier.sallou@irisa.fr',
-    'version': '3.0.26',
+    'version': '3.0.27',
      'classifiers': [
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
